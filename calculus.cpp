0a0925e8ceab6afc14ea1cff2aaed1d6c3a11374
TaXuanTung21020400 <100212486+TaXuanTung21020400@users.noreply.github.com>
2022-03-07 21:30:48 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -7,9 +7,11 @@ using std::cout;
 using std::endl;
 using std::stod;
 
+
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+
 /***
     Args:
         x (double): a number
@@ -52,67 +54,6 @@ double mySqrt(double x) {
 } 
     return sqrt(x);
 }
-}
 
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
-double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
-    }
 
-
-    return 0;
-} 
-    return sqrt(x);
-}
-
-
-double mySin(double x);
-double myCos(double x);
-double mySqrt(double x);
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
-double myCos(double x) 
-{
-    return 0.0;
-}
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
-double mySin(double x)
-{
-    return 0.0;
-}
-
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
-double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
-    }
-
-    
-    return 0;
-}
